f89f02b684e357eeeaaa0b1f905e84fa645c6644
aravindanirudh <aravindanirudh1@gmail.com>
2026-07-15 20:38:19 +0530

    26:35 of tutorial completed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hello from '../components/hello';
 
 const Home = () => {
-  console.log('Home page rendered!');
   return (
     <main>
       <div className="underline">Welcome to Next.js!</div>
--- components/hello.tsx
+++ components/hello.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 const Hello = () => {
-    console.log('I am a client component!');
   return (
     <div>Hello</div>
   )
