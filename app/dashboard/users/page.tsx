38a8d3f31becd2824f13f10442bf65ceec27d92a
aravindanirudh <aravindanirudh1@gmail.com>
2026-07-15 22:10:39 +0530

    30:23 of tutorial completed

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Link from 'next/link'
 
 const Users = () => {
   return (
-    <div>Users</div>
+    <div>
+      <h1>Dashboard Users</h1>
+      <ul className="mt-10">
+        <li className="mb-2"><Link href="/dashboard/users/1">User 1</Link></li>
+        <li className="mb-2"><Link href="/dashboard/users/2">User 2</Link></li>
+        <li className="mb-2"><Link href="/dashboard/users/3">User 3</Link></li>
+        <li className="mb-2"><Link href="/dashboard/users/4">User 4</Link></li>
+      </ul>
+    </div>
   )
 }
 
